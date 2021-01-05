680bcd442516b27e0fcc89ba08b93d0da790758a
Наумов Константин Григорьевич <naumov_kg@ecosafety.ru>
2021-01-05 19:18:49 +0300

    initial commit

---
+++ src/components/Search.js
@@ -0,0 +1,40 @@
+import React, {useContext, useState} from 'react';
+import { AlertContext } from '../context/alert/alertContext';
+import { GitHubContext } from '../context/github/githubContext';
+
+export const Search = () => {
+
+  const [value, setValue] = useState('')
+
+  const alert = useContext(AlertContext);
+
+  const github = useContext(GitHubContext)
+
+  const onSubmit = event => {
+    if(event.key !== 'Enter') {
+      return
+    }
+
+    github.clearUsers()
+    
+    if(value.trim()) {
+      alert.hide()
+      github.search(value.trim())
+    } else {
+      alert.show('Введите данные')
+    }
+  }
+
+  return (
+    <div className="form-group">
+      <input 
+        type="text"
+        className="form-control"
+        placeholder="введите ник пользователя..."
+        value={value}
+        onChange={event => setValue(event.target.value)}
+        onKeyPress={onSubmit}
+      />
+    </div>
+  )
+}
